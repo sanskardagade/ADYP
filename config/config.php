ed8f83af9c3bf0988d487a4310073c1087ef5031
sanskardagade <sanskardagade07@gmail.com>
2026-07-01 22:23:56 +0530

    first commit

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Project configuration
+define('APP_NAME', 'Staff Management System')
+define('BASE_URL', 'http://localhost/StaffManagement');
+date_default_timezone_set('Asia/Kolkata');
+
+//Show errors while reporting 
+error_reporting(E_ALL);
+ini
