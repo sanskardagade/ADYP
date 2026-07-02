38b9a614b146fd3bc77b0978cbab4a3fe4dcd133
sanskardagade <sanskardagade07@gmail.com>
2026-07-02 15:19:20 +0530

    backend

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
 
 //Project configuration
-define('APP_NAME', 'Staff Management System')
+define('APP_NAME', 'Staff Management System');
 define('BASE_URL', 'http://localhost/StaffManagement');
 date_default_timezone_set('Asia/Kolkata');
 
 //Show errors while reporting 
 error_reporting(E_ALL);
-ini
+ini_set('display_errors', 1);   
